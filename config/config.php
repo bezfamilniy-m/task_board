0e0a2e805c2633a981f1a10120b90af2f0429c96
moshe <9777953667@mail.ru>
2019-10-12 01:36:36 +0300

    add config and controllers

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+define('DEVELOPMENT',true);
+
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvctest');
+define('DB_USER', 'root');
+define('DB_PASS', 'qwerty101');
+
+ 
+define('BASEPATH', 'http://localhost/test_task/php-mvc-example/');
+define('MODELPATH','models/');
+define('CONTROLLERPATH','controllers/');
+define('VIEWPATH','views/');
+define('LIBPATH','libs/');
+ 
+define('DEFAULTCONTROLLER', 'main');
+ 
+
+DEFINE('SESSION_SALT',  'SALT_FOR_SESSION');
